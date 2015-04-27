c044079dadec3b17a8043e94d43d86ec281af180
chrsjxn <chjackson@zendesk.com>
2015-04-27 14:22:46 -0700

    Move route code out of index.js

---
+++ js/routes.js
@@ -0,0 +1,36 @@
+module.exports = function(app) {
+  var sse = require('./tags');
+
+  app.get('/', function (request, response) {
+    response.redirect('https://github.com/chrsjxn/massive-wookie');
+  });
+
+  app.get('/api/:username', function(request, response) {
+    response.send(sse.getTags(request.params.username));
+  });
+
+  app.get('/api/:username/:tag', function(request, response) {
+    var username = request.params.username,
+        tag = request.params.tag;
+
+    response.send(sse.hasTag(username, tag));
+  });
+
+  app.put('/api/:username/:tag', function(request, response) {
+    var username = request.params.username,
+        tag = request.params.tag;
+
+    sse.addTag(username, tag);
+
+    response.sendStatus(200);
+  });
+
+  app.delete('/api/:username/:tag', function(request, response) {
+    var username = request.params.username,
+        tag = request.params.tag;
+
+    sse.removeTag(username, tag);
+
+    response.sendStatus(200);
+  });
+};
