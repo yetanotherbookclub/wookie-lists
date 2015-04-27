aaac9c2d1f5ae0fc0ec0ff80dfe6e729f5dcc22c
chrsjxn <chjackson@zendesk.com>
2015-04-27 16:26:47 -0700

    Simple query function

--- js/tags.js
+++ js/tags.js
@@ -5,47 +5,39 @@ function extractTags(row) {
   return row.tag;
 };
 
+function query(queryString, params, formatResponse) {
+  pg.connect(DATABASE_URL, function(err, client, done) {
+    client.query(queryString, params, function(error, result) {
+      done();
+
+      if (err) {
+        console.error(error); response.send('Error: ' + error);
+      } else {
+        formatResponse(result);
+      }
+    });
+  });
+}
+
 module.exports = {
   getTags: function(username, response) {
-    pg.connect(DATABASE_URL, function(err, client, done) {
-      client.query('SELECT * FROM user_tags WHERE username=$1;', [username], function(error, result) {
-        done();
-
-        if (err) {
-          console.error(error); response.send(error);
-        } else {
-          response.send({
-            username: username,
-            tags: result.rows.map(extractTags)
-          });
-        }
-      });
+    query('SELECT * FROM user_tags WHERE username=$1;', [username], function(result) {
+      response.send({
+        username: username,
+        tags: result.rows.map(extractTags)
+      })
     });
   },
 
   addTag: function(username, tag, response) {
-    pg.connect(DATABASE_URL, function(err, client, done) {
-      client.query('INSERT INTO user_tags VALUES ($1, $2);', [username, tag], function(error, result) {
-        done();
-        if (err) {
-          console.error(error); response.send(error);
-        } else {
-          response.sendStatus(200);
-        }
-      });
+    query('INSERT INTO user_tags VALUES ($1, $2);', [username, tag], function(result) {
+      response.sendStatus(200);
     });
   },
 
   removeTag: function(username, tag, response) {
-    pg.connect(DATABASE_URL, function(err, client, done) {
-      client.query('DELETE FROM user_tags WHERE username=$1 AND tag=$2;', [username, tag], function(error, result) {
-        done();
-        if (err) {
-          console.error(error); response.send(error);
-        } else {
-          response.sendStatus(200);
-        }
-      });
+    query('DELETE FROM user_tags WHERE username=$1 AND tag=$2;', [username, tag], function(result) {
+      response.sendStatus(200);
     });
   }
 };
