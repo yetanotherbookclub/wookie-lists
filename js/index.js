b9e99c400329bae981fe2ff7ada64a2cc59309e7
chrsjxn <chjackson@zendesk.com>
2015-04-27 16:44:32 -0700

    Move index.js into the js dir

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
-var setupRoutes = require('./js/routes');
+var setupRoutes = require('./routes');
 
 setupRoutes(app);
 
